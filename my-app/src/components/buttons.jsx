96db4f478797175537c6b547d384d090fae84174
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-11-03 18:04:59 +0000

    Buttons responsive

--- my-app/src/components/buttons.jsx
+++ my-app/src/components/buttons.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Button from "./buttons/button-slope-filter";
+import Button1 from "./buttons/button-slope";
+import Button2 from "./buttons/button-filter";
 import '../styles/button-container.css'
 
 const Buttons = () => {
 
     return (
         <div className="buttons-container">
-            <Button title="Slope" />
-            <Button title="Filter On" />
-
+            <Button1 title="Slope" />
+            <Button2 title="Filter" />
         </div>
     );
 }
