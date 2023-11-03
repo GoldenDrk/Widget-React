c67a167d6e90ad4ded2959f4b3a8e4eda79c2b5b
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-11-03 17:37:00 +0000

    Radio buttons responsive

--- my-app/src/components/pass.jsx
+++ my-app/src/components/pass.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import RadioPass from "./buttons/radio-pass";
+import '../styles/radio-pass.css'
 import '../styles/passStyle.css'
 
 const Pass = () => {
 
+    const [ low, high ] = useState("Lowpass")
+
+    const update = () => {
+        high("Highpass")
+    }
+
+    const midUpdate = () => {
+        high("Midpass")
+    }
+
+    const unUpdate = () => {
+        high("Lowpass")
+    }
+
     return (
         <div>
             <div className="container-pass">
-                <h2>Lowpass</h2>
+                <h2 className="pass-title">{low}</h2>
             </div>
             <div className="container-radio-pass">
-                <RadioPass title="6" />
-                <RadioPass title="12" />
-                <RadioPass title="18" />
-                <RadioPass title="24" />
-                <RadioPass title="36" />
+                <div className="radio-low" onClick={() => unUpdate()}>
+                    <RadioPass title="6" />
+                    <RadioPass title="12" />
+                </div>
+                <div onClick={() => midUpdate()}>
+                    <RadioPass title="18" />
+                </div>
+                <div className="radio-high" onClick={() => update()}>
+                    <RadioPass title="24" />
+                    <RadioPass title="36" />
+                </div>
             </div>
         </div>
     );
